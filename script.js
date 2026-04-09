f2a7e98c6d813b967fff3b0e5f3557533704cb8d
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-04-09 10:50:01 +0800

    create a operate function that takes two numbers and an operator

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 console.log(add(1, 4));
+
+let firstNumber, secondNumber, operator;
+
+function operate (firstNumber, secondNumber, operator) {
+  return operator(firstNumber, secondNumber); 
+} 
+
+console.log(operate(1, 6, add));
