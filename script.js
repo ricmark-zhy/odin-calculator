2d28f0cce25cf7fa5645ef50d48386ff60c93a1f
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-04-12 15:00:47 +0800

    created a function that return the button clicked and handles decimal inputs

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const operators = {
 let num1 = 4, num2 = 10;
 
 function operate(num1, num2, operator){
+  num1 = Number(num1);
+  num2 = Number(num2);
   return operator(num1, num2);
 }
 
@@ -21,3 +23,46 @@ let result = operate(num1, num2, operator);
 console.log(result)
 
 //create a function that update the display when number buttons are click
+
+const calculator = document.querySelector('#calculator');
+const display = document.querySelector('#display');
+
+let firstNum = '', secondNum = '';
+
+calculator.addEventListener('click', event => {
+  //if target is not a button, cancel
+  if(!event.target.closest('button')){
+    return;
+  }
+  const button = event.target.textContent;
+
+  if (isNumber(button)){
+
+    firstNum += getValue(button, firstNum);
+    display.value = firstNum;
+  }
+});
+
+
+// helper functions
+
+let getValue = (button, storage) => {
+  
+  if (button == '.'){ //handles decimal
+    if (!storage){
+      return '0.'; //convert to this when decimal are entered first
+    } else if (storage.includes('.')){
+      return ''; //return nothing if decimal are already present.
+    }
+  } else {
+    return button;
+  }
+
+}
+
+const numbers = '1234567890.';
+let isNumber = value => {
+  return numbers.includes(value);
+}
+
+
