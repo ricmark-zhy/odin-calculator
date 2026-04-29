e1995de33bff42be01d4b210eb6801dedf87aa88
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-04-29 11:13:21 +0800

    add update display function when numbers are clicked

--- calculator -v2.0/calc2.js
+++ calculator -v2.0/calc2.js
@@ -14,3 +14,83 @@ function operate (num1, num2, operator){
   num2 = Number(num2);
   return operator(num1, num2).toString();
 }
+
+let firstNum = '', secondNum = '', input = '';
+let shiftToSecond = false;
+
+calculator.addEventListener('click', event => {
+  
+  if (!event.target.closest('button')){
+    return;
+  }
+
+  const button = event.target.id;
+
+  handleClick(button);
+
+});
+
+function handleClick(button){
+  if (isNumber(button)){
+    handleNum(button);
+  } else if (button === '.'){
+    handleDecimal();
+  } else if (button === 'sign'){
+    changeSign();
+  }
+}
+
+function handleNum(button){
+  input += button;
+  if (!shiftToSecond){
+    firstNum += input;
+  } else {
+    secondNum += input;
+  }
+  updateDisplay();
+}
+
+function handleDecimal(){
+  if (!input){
+    input = '0.';
+  } else if (input.includes('.')){
+    input += '';
+  } else {
+    input += '.';
+  }
+  updateDisplay();
+}
+
+function changeSign(){
+  if (!input){
+    input = '-0'
+  } else if (input.includes('-')){
+    input = input.slice(1);
+  } else {
+    input = '-' + input;
+  }
+  updateDisplay();
+}
+
+function updateDisplay(){
+  if (!secondNum){
+    firstNum = input;
+    curr_operand.textContent = firstNum;
+  } else {
+    
+  }
+}
+
+
+
+const digits = '123456789';
+
+let isNumber = (button) => {
+  if (digits.includes(button)){
+    return true;
+  }
+  return false;
+}
+
+let a = '9';
+console.log(isNumber(a));
