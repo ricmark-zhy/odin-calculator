c05b98878ecb03862c471c6c2dde806dc3213725
RicMark <158183787+ricmark-zhy@users.noreply.github.com>
2026-05-04 03:55:35 +0800

    lost track, finally made it work the way I want -_-

--- calculator -v2.0/calc2.js
+++ calculator -v2.0/calc2.js
@@ -2,95 +2,170 @@ const calculator = document.querySelector('#calculator');
 const curr_operand = document.querySelector('#current_operand');
 const prev_operand = document.querySelector('#previous_operand');
 
-const operators = {
-  plus: (num1, num2) => num1 + num2,
-  minus: (num1, num2) => num1 - num2,
-  divide: (num1, num2) => num1 / num2,
-  times: (num1, num2) => num1 * num2,
-}
-
-function operate (num1, num2, operator){
-  num1 = Number(num1);
-  num2 = Number(num2);
-  return operator(num1, num2).toString();
-}
-
 let firstNum = '', secondNum = '', input = '';
 let shiftToSecond = false;
+let currentOperator = '';
 
 calculator.addEventListener('click', event => {
-  
-  if (!event.target.closest('button')){
-    return;
+  if (event.target.closest('button')){
+    let button = event.target.id;
+    if (!isNaN(button) || button === '.'){
+      handleNumber(button);
+      updateDisplay();
+    } else if (isOperator(button) || button === 'equals'){
+      chooseOperator(button);
+      updateDisplay();
+    } else if (button === 'delete'){
+      del();
+      updateDisplay();
+    } else if (button === 'clear'){
+      clear();
+      updateDisplay();
+    } else if (button === 'sign'){
+      changeSign();
+      updateDisplay();
+    }
   }
-
-  const button = event.target.id;
-
-  handleClick(button);
-
 });
 
-function handleClick(button){
-  if (isNumber(button)){
-    handleNum(button);
-  } else if (button === '.'){
-    handleDecimal();
-  } else if (button === 'sign'){
-    changeSign();
-  }
-}
+function handleNumber(button){
 
-function handleNum(button){
-  input += button;
   if (!shiftToSecond){
-    firstNum += input;
+    if (firstNum.includes('.') && button === '.'){
+      return;
+    }
+    firstNum += button;
   } else {
-    secondNum += input;
+    if (secondNum.includes('.') && button === '.'){
+      return
+    }
+    secondNum += button;
   }
-  updateDisplay();
-}
-
-function handleDecimal(){
-  if (!input){
-    input = '0.';
-  } else if (input.includes('.')){
-    input += '';
-  } else {
-    input += '.';
-  }
-  updateDisplay();
-}
-
-function changeSign(){
-  if (!input){
-    input = '-0'
-  } else if (input.includes('-')){
-    input = input.slice(1);
-  } else {
-    input = '-' + input;
-  }
-  updateDisplay();
 }
 
 function updateDisplay(){
-  if (!secondNum){
-    firstNum = input;
+  if (!shiftToSecond){
+    prev_operand.textContent = '';
     curr_operand.textContent = firstNum;
   } else {
-    
+    prev_operand.textContent = firstNum + " " + currentOperator;
+    curr_operand.textContent = secondNum;
   }
 }
 
+function handleEquals() {
+  if (currentOperator && secondNum) {
+    let temp = firstNum;
+    operate(currentOperator);
+    if (firstNum === Infinity) {
+      handleInfinity();
+      return;
+    }
+    shiftToSecond = false;
+    currentOperator = '';
+    prev_operand.textContent = '';
+    secondNum = '';
+  }
+}
 
+function chooseOperator(button){
+  if (button === 'equals'){
+    handleEquals();
+    return;
+  }
 
-const digits = '123456789';
+  shiftToSecond = true;
 
-let isNumber = (button) => {
-  if (digits.includes(button)){
+  if (!secondNum){
+    currentOperator = button;
+  } else {
+    let temp = firstNum;
+    operate(currentOperator);
+    if (firstNum === Infinity){
+      handleInfinity();
+      return;
+    }
+    currentOperator = button;
+    secondNum = '';
+  }
+}
+
+function operate (operator){
+  let a = Number(firstNum);
+  let b = Number(secondNum);
+
+  switch (operator){
+    case '+':
+      firstNum = a + b;
+      break;
+    case '÷':
+      firstNum = a / b;
+      break;
+    case '×':
+      firstNum = a * b;
+      break;
+    case '−':
+      firstNum = a - b;
+      break;
+  }
+  if (firstNum === Infinity){
+    return;
+  }
+
+  firstNum = firstNum.toString();
+}
+
+function handleInfinity(){
+  
+  alert("you can't divide a thing by nothing you silly kid!");
+  shiftToSecond = false;
+  firstNum = '';
+  secondNum = '';
+}
+
+function del(){
+  if (!secondNum){
+    currentOperator = '';
+  }
+  if (!shiftToSecond){
+    firstNum = firstNum.slice(0, -1);
+  } else {
+    secondNum = secondNum.slice(0, -1);
+  }
+  if (!currentOperator){
+    shiftToSecond = false;
+  }
+}
+
+function clear(){
+  shiftToSecond = false;
+  firstNum = '';
+  secondNum = '';
+  currentOperator = '';
+  prev_operand.textContent = '';
+  curr_operand.textContent = '';
+}
+
+function changeSign(){
+  if (!shiftToSecond){
+    if (firstNum.includes('-')){
+      firstNum = firstNum.slice(1);
+    } else {
+      firstNum = '-' + firstNum;
+    }
+  } else {
+    if (secondNum.includes('-')){
+      secondNum = secondNum.slice(1);
+    } else {
+      secondNum = '-' + secondNum;
+    }
+  }
+}
+
+let operators = '÷×+−';
+let isOperator = (operator) => {
+  if (operators.includes(operator)){
     return true;
   }
   return false;
 }
-
-let a = '9';
-console.log(isNumber(a));
